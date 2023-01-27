bf83ce92613e4530f54f03e7a10899bd60ca6f09
Jonas Eichenberger <jo.eichenberger@gmail.com>
2023-01-27 15:21:51 +0100

    small cleanup

--- simulation-trunk/src/main.rs
+++ simulation-trunk/src/main.rs
@@ -1,105 +1,19 @@
 use lib_simulation as sim;
 use rand::prelude::*;
 use std::cell::RefCell;
-use std::f32::consts::PI;
+use std::f64::consts::PI;
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::CanvasRenderingContext2d;
-
-#[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: String);
-}
-
-#[wasm_bindgen]
-pub fn greet(number_string: String) {
-    alert(format!("From Jonas: {}!", number_string));
-}
-
-pub fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    x: f64,
-    y: f64,
-    size: f64,
-    rotation: f64,
-) {
-    context.begin_path();
-    context.move_to(
-        x - rotation.sin() * size * 1.5,
-        y + rotation.cos() * size * 1.5,
-    );
-    context.line_to(
-        x - (rotation + 2.0 / 3.0 * PI as f64).sin() * size,
-        y + (rotation + 2.0 / 3.0 * PI as f64).cos() * size,
-    );
-    context.line_to(
-        x - (rotation + 4.0 / 3.0 * PI as f64).sin() * size,
-        y + (rotation + 4.0 / 3.0 * PI as f64).cos() * size,
-    );
-    context.line_to(
-        x - rotation.sin() * size * 1.5,
-        y + rotation.cos() * size * 1.5,
-    );
-
-    context.stroke()
-}
-
-pub fn draw_square(context: &web_sys::CanvasRenderingContext2d, x: f64, y: f64, size: f64) {
-    context.fill_rect(x, y, size, size);
-}
-
-pub fn draw_circle(context: &web_sys::CanvasRenderingContext2d, x: f64, y: f64, radius: f64) {
-    context.arc(x, y, radius, 0.0, 2.0 * PI as f64);
-}
-
-pub fn redraw(
-    simulation: &mut sim::Simulation,
-    context: &mut CanvasRenderingContext2d,
-    viewport_width: f64,
-    viewport_height: f64,
-    viewport_scale: f64,
-) {
-    context.clear_rect(0.0, 0.0, viewport_width, viewport_height);
-
-    let mut rng = thread_rng();
-    simulation.step(&mut rng);
-
-    // draw food
-    for food in &simulation.world().foods {
-        context.begin_path();
-        context.arc(
-            food.position().x * viewport_width,
-            food.position().y * viewport_height,
-            5.0,
-            0.0,
-            2.0 * PI as f64,
-        );
-        context.set_fill_style(&"rgb(0, 255, 128)".into());
-        context.fill();
-    }
-
-    // draw moving birds
-    for animal in &simulation.world().animals {
-        draw_triangle(
-            &context,
-            animal.position().x * viewport_width,
-            animal.position().y * viewport_height,
-            5.0 * viewport_scale,
-            animal.rotation.angle(),
-        );
-        context.set_fill_style(&"rgb(255, 255, 255)".into());
-        context.fill();
-    }
-}
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
 
     let mut rng = thread_rng();
     let simulation = sim::Simulation::new();
-    let world = simulation.world();
-    log::info!("{:?}", world);
+    //let world = simulation.world();
+    //log::info!("{:?}", world);
 
     let document = web_sys::window().unwrap().document().unwrap();
 
@@ -108,7 +22,7 @@ fn main() {
     let simulation_clone = simulation.clone();
 
     let train = Closure::wrap(Box::new(move || {
-        web_sys::console::log_1(&"Button train clicked".into());
+        log::info!("Button train clicked");
 
         let mut simulation = simulation_clone.borrow_mut();
         let statistics = simulation.train(&mut rng);
@@ -120,7 +34,7 @@ fn main() {
             statistics.avg_fitness()
         );
 
-        web_sys::console::log_1(&stats_formated.into());
+        log::info!("{:?}", stats_formated)
     }) as Box<dyn FnMut()>);
 
     document
@@ -179,9 +93,95 @@ fn main() {
     //context.stroke();
 }
 
+pub fn draw_triangle(
+    context: &web_sys::CanvasRenderingContext2d,
+    x: f64,
+    y: f64,
+    size: f64,
+    rotation: f64,
+) {
+    context.begin_path();
+    context.move_to(
+        x - rotation.sin() * size * 1.5,
+        y + rotation.cos() * size * 1.5,
+    );
+    context.line_to(
+        x - (rotation + 2.0 / 3.0 * PI as f64).sin() * size,
+        y + (rotation + 2.0 / 3.0 * PI as f64).cos() * size,
+    );
+    context.line_to(
+        x - (rotation + 4.0 / 3.0 * PI as f64).sin() * size,
+        y + (rotation + 4.0 / 3.0 * PI as f64).cos() * size,
+    );
+    context.line_to(
+        x - rotation.sin() * size * 1.5,
+        y + rotation.cos() * size * 1.5,
+    );
+
+    context.stroke()
+}
+
+pub fn draw_square(context: &web_sys::CanvasRenderingContext2d, x: f64, y: f64, size: f64) {
+    context.fill_rect(x, y, size, size);
+}
+
+pub fn draw_circle(context: &web_sys::CanvasRenderingContext2d, x: f64, y: f64, radius: f64) {
+    context.arc(x, y, radius, 0.0, 2.0 * PI as f64);
+}
+
+pub fn redraw(
+    simulation: &mut sim::Simulation,
+    context: &mut CanvasRenderingContext2d,
+    viewport_width: f64,
+    viewport_height: f64,
+    viewport_scale: f64,
+) {
+    context.clear_rect(0.0, 0.0, viewport_width, viewport_height);
+
+    let mut rng = thread_rng();
+    simulation.step(&mut rng);
+
+    // draw food
+    for food in &simulation.world().foods {
+        context.begin_path();
+        context.arc(
+            food.position().x * viewport_width,
+            food.position().y * viewport_height,
+            5.0,
+            0.0,
+            2.0 * PI,
+        );
+        context.set_fill_style(&"rgb(0, 255, 128)".into());
+        context.fill();
+    }
+
+    // draw moving birds
+    for animal in &simulation.world().animals {
+        draw_triangle(
+            &context,
+            animal.position().x * viewport_width,
+            animal.position().y * viewport_height,
+            5.0 * viewport_scale,
+            animal.rotation.angle(),
+        );
+        context.set_fill_style(&"rgb(255, 255, 255)".into());
+        context.fill();
+    }
+}
+
 fn request_animation_frame(f: &Closure<dyn FnMut()>) {
     web_sys::window()
         .unwrap()
         .request_animation_frame(f.as_ref().unchecked_ref())
         .expect("should register `requestAnimationFrame` OK");
+}
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: String);
+}
+
+#[wasm_bindgen]
+pub fn greet(number_string: String) {
+    alert(format!("From Jonas: {}!", number_string));
 }
